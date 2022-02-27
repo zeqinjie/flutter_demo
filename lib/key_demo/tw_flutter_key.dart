80bf4427c8535133267bb73894d9c135369cc2e2
郑泽钦 <zhengzeqin@addcn.com>
2022-02-27 18:44:18 +0800

    <add>(key): key      ■ ValueKey     ■ ObjectKey     ■ UniqueKey

--- lib/key_demo/tw_flutter_key.dart
+++ lib/key_demo/tw_flutter_key.dart
@@ -1,40 +1,74 @@
 import 'package:flutter/material.dart';
 
 /// create by:  zhengzeqin
-/// create time:  2021-12-19 14:29 
+/// create time:  2021-12-19 14:29
 /// des:  https://www.bilibili.com/video/BV15k4y1B74z?spm_id_from=333.999.0.0
 
 class TWFlutterKey extends StatelessWidget {
-  const TWFlutterKey({Key? key}) : super(key: key);
 
+  const TWFlutterKey({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'key Demo',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("key demo"),),
-        body: Column(
-          children: const [
-            _Box(
-              color: Colors.red,
-            ),
-            _Box(
-              color: Colors.grey,
-            ),
-            _Box(
-              color: Colors.blue,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text("key demo"),
+        ),
+        body: Center(
+          child: _buildRowBox(),
         ),
       ),
     );
   }
 
+  Widget _buildColumnBox() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _Box(
+          key: UniqueKey(),
+          color: Colors.grey,
+        ),
+        _Box(
+          // key: ValueKey("red"),
+          color: Colors.red,
+        ),
+        _Box(
+          // key: ValueKey("blue"),
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRowBox() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        _Box(
+          // key: ValueKey("grey"),
+          color: Colors.grey,
+        ),
+        _Box(
+          // key: ValueKey("red"),
+          color: Colors.red,
+        ),
+        _Box(
+          // key: ValueKey("blue"),
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
 }
 
 class _Box extends StatefulWidget {
   final Color color;
+  final width = 100.0;
+  final height = 100.0;
+
   const _Box({Key? key, this.color = Colors.yellow}) : super(key: key);
 
   @override
@@ -43,6 +77,7 @@ class _Box extends StatefulWidget {
 
 class _CountState extends State<_Box> {
   int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -50,6 +85,9 @@ class _CountState extends State<_Box> {
         _incrementCounter();
       },
       child: Container(
+        alignment: Alignment.center,
+        width: widget.width,
+        height: widget.height,
         color: widget.color,
         child: Text("count:$_counter"),
       ),
@@ -62,6 +100,3 @@ class _CountState extends State<_Box> {
     });
   }
 }
-
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:tw_chart_demo/chart_demo/tw_chart_app.dart';
+import 'package:tw_chart_demo/key_demo/tw_flutter_key.dart';
 
 void main() {
-  runApp(const TWChartApp());
+  runApp(const TWFlutterKey());
 }
 
 
